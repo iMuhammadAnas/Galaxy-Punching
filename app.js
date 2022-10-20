68e133e46f707f7cf6bb6eee84ceb5671b1697cc
Muhammad Anas <anasnaseem420@gmail.com>
2022-10-20 19:22:36 +0500

    Adding all Files

---
+++ app.js
@@ -0,0 +1,43 @@
+const footer = document.getElementById('footer');
+footer.innerHTML=`
+<div class="footer-links">
+            <div class="f-link-1">
+                <div class="f-heading">
+                    <p class="f-heading-1">Services</p>
+                </div>
+                <div class="footer-ser">
+                    <ul>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Embroidery Digitizing</a></li>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Vectorizing</a></li>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Color Separation</a></li>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Custom Patches</a></li>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Picture Editing</a></li>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Illustration/Logo Creation</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="f-link-2">
+                <div class="f-heading">
+                    <p class="f-heading-1">Galaxy Punching</p>
+                </div>
+                <div class="footer-pages">
+                    <ul>
+                        <li><a href="digitizing.html"><i class="angle-icon fa-solid fa-angles-right"></i> Digitizng</a></li>
+                        <li><a href="vectorizing.html"><i class="angle-icon fa-solid fa-angles-right"></i> Vectorizing</a></li>
+                        <li><a href="gallery.html"><i class="angle-icon fa-solid fa-angles-right"></i> Our Works</a></li>
+                        <li><a href="#"><i class="angle-icon fa-solid fa-angles-right"></i> Blog</a></li>
+                        <li><a href="contact.html"><i class="angle-icon fa-solid fa-angles-right"></i> Contact Us</a></li>
+                        <li><a href="about us.html"><i class="angle-icon fa-solid fa-angles-right"></i> About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-pay">
+                <div class="pay-1">
+                    <img src="./Images/fimg3.png">
+                    <img src="./Images/fimg2.png">
+                </div>
+                <img src="./Images/payment.png" width="100%">
+                <p class="copy-right">© Copyright 2020 Galaxy Punching. All Rights Reserved.</p>
+                <img src="./Images/12.png">
+        </div>`
